70743e1ebb24bb28f34b666569edbcc67922fde7
shug <vxzyfx@gmail.com>
2025-09-11 15:03:07 +0800

    fix: decode value

--- snap_api/src/service/data/mod.rs
+++ snap_api/src/service/data/mod.rs
@@ -1,5 +1,5 @@
-use influxdb2_structmap::GenericMap;
 use common_define::decode::Value;
+use influxdb2_structmap::GenericMap;
 
 pub(crate) struct DataService;
 
@@ -18,27 +18,41 @@ pub struct DeviceData {
 impl influxdb2::FromMap for DeviceData {
     fn from_genericmap(map: GenericMap) -> Self {
         let mut map = map;
-        let mut this = Self::default();
-        if let Some(influxdb2_structmap::value::Value::String(id)) = map.remove("id") {
-            this.id = id.parse::<i64>().unwrap_or_default();
-        }
-        if let Some(influxdb2_structmap::value::Value::String(name)) = map.remove("name") {
-            this.name = name;
-        }
-        if let Some(influxdb2_structmap::value::Value::String(unit)) = map.remove("unit") {
-            this.unit = unit;
-        }
-        if let Some(value) = map.remove("value") {
-            match value {
-                influxdb2_structmap::value::Value::Long(i) => { this.value = Value::Int(i); }
-                influxdb2_structmap::value::Value::Double(f) => { this.value = Value::Float(f.0); }
-                influxdb2_structmap::value::Value::Bool(b) => { this.value = Value::Bool(b); }
-                _ => {}
-            }
-        }
-        if let Some(influxdb2_structmap::value::Value::TimeRFC(time)) = map.remove("_time") {
-            this.time = time.timestamp_millis();
-        }
-        this
+        let id = if let Some(influxdb2_structmap::value::Value::String(id)) = map.remove("id") {
+            id.parse::<i64>().unwrap_or_default()
+        } else {
+            0
+        };
+        let name = if let Some(influxdb2_structmap::value::Value::String(name)) = map.remove("name")
+        {
+            name
+        } else {
+            String::new()
+        };
+        let unit = if let Some(influxdb2_structmap::value::Value::String(unit)) = map.remove("unit")
+        {
+            unit
+        } else {
+            String::new()
+        };
+        let value = map
+            .remove("value")
+            .or_else(|| map.remove("f_value"))
+            .or_else(|| map.remove("b_value"))
+            .unwrap_or(influxdb2_structmap::value::Value::Long(0));
+        let value = match value {
+            influxdb2_structmap::value::Value::Long(i) => Value::Int(i),
+            influxdb2_structmap::value::Value::Double(f) => Value::Float(f.0),
+            influxdb2_structmap::value::Value::Bool(b) => Value::Bool(b),
+            _ => Value::default(),
+        };
+        let time =
+            if let Some(influxdb2_structmap::value::Value::TimeRFC(time)) = map.remove("_time") {
+                time.timestamp_millis()
+            } else {
+                0
+            };
+        Self { id, name, unit, value, time }
     }
 }
+
